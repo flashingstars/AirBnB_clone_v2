cf227d169b5e3d7d92f158c961e5809983f4675f
Francis Onyach <franblog7@gmail.com>
2023-03-21 22:23:38 -0700

    db for the project

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, DateTime, ForeignKey
 
 
 class City(BaseModel):
