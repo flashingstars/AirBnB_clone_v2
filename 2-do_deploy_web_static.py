1b4a90ab64b867f25c193decb8cae8d6c2ef87a7
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-06 13:48:50 -0700

    Archiving to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,19 +8,20 @@ env.hosts = ["54.236.56.204", "18.204.15.121"]
 
 
 def do_deploy(archive_path):
-
-    if not os.path.isfile(archive_path):
+    """ A function that deploys and decompresses code """
+    
+if not os.path.isfile(archive_path):
         return False
 
-    file_path = archive_path.split("/")[-1]
+    file_path1 = archive_path.split("/")[-1]
     dir_name = file_path.split(".")[0]
 
     dir_path = "/data/web_static/releases/{}".format(dir_name)
     try:
         put(archive_path, "/tmp/")
         run("sudo mkdir -p {}".format(dir_path))
-        run("sudo tar -xvzf /tmp/{} -C {}".format(file_path, dir_path))
-        run("sudo rm /tmp/{}".format(file_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(file_path1, dir_path))
+        run("sudo rm /tmp/{}".format(file_path1))
         run("sudo mv {}/web_static/* {}".format(dir_path, dir_path))
         run("sudo rm -rf {}/web_static".format(dir_path))
         run("sudo rm -rf /data/web_static/current")
