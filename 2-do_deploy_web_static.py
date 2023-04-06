bc861eb1719514c55f76d5e6b5c26e68c47efcbc
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-06 13:35:01 -0700

    Archiving to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
-""" A script to send an archived file to a remote server
-and decompress it"""
+"""Deploying the webstatic to the server
+"""
+from fabric.api import env, put, run
+import os
 
-
-from fabric.api import run, env, put
-import os.path 
-
-env.hosts = ['54.236.56.204', '18.204.15.121']
-env.key_filename = '~/.ssh/school'
-env.user = 'ubuntu'
+env.hosts = ["54.236.56.204", "18.204.15.121"]
 
 
 def do_deploy(archive_path):
-    """ A function that deploys code and decompresses it"""
 
     if not os.path.isfile(archive_path):
         return False
-    compressed_f1 = archive_path.split("/")[-1]
-    minus_extension = compressed_f1.split(".")[0]
 
-         try:
-            remote_path = "/data/web_static/releases/{}/".format(no_extension)
-            symbolic_link = "/data/web_static/current"
-            put(archive_path, "/tmp/")
-            run("sudo mkdir - p {}".format(remote_path))
-            run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_f1, remote_path))
-            run("sudo rm /tmp/{}".format(compressed_f1))
-            run("sudo mv {}/web_static/* {}".format(remote_path))
-            run("sudo rm -rf {}/web_static">format(remote_path))
-            run("sudo rm -rf /data/web_static/current")
-            run("sudo ln -sf {} {}".format(remote_path, symbolic_link))
-            return True
-            except Exception as e:
-                return False
+    file_path = archive_path.split("/")[-1]
+    dir_name = file_path.split(".")[0]
+
+    dir_path = "/data/web_static/releases/{}".format(dir_name)
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(dir_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(file_path, dir_path))
+        run("sudo rm /tmp/{}".format(file_path))
+        run("sudo mv {}/web_static/* {}".format(dir_path, dir_path))
+        run("sudo rm -rf {}/web_static".format(dir_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} /data/web_static/current".format(dir_path))
+    except Exception as e:
+        return False
+    return True
