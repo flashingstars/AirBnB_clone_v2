40db6f4d8aebf79b5eb2b41a00990b0ff30af630
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-06 06:05:26 -0700

    Distributing an archiv to the web servers

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" A script to send an archived file to a remote server
+and decompress it"""
+
+
+from fabric.api import run, env, put
+import os.path 
+
+env.hosts = ['54.236.56.204', '18.204.15.121']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
+
+
+def do_deploy(archive_path):
+    """ A function that deploys code and decompresses it"""
+
+    if not os.path.isfile(archive_path):
+        return False
+    compressed_f1 = archive_path.split("/")[-1]
+    minus_extension = compressed_f1.split(".")[0]
+
+         try:
+            remote_path = "/data/web_static/releases/{}/".format(no_extension)
+            symbolic_link = "/data/web_static/current"
+            put(archive_path, "/tmp/")
+            run("sudo mkdir - p {}".format(remote_path))
+            run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_f1, remote_path))
+            run("sudo rm /tmp/{}".format(compressed_f1))
+            run("sudo ln -sf {} {}".format(remote_path, symbolic_link))
+            return True
+            except Exception as e:
+                return False
