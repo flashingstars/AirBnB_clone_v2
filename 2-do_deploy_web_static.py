4a849f74875781f1b57632a279a788b8c2472ff2
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-07 04:14:36 -0700

    Deploying archived files

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,28 +4,36 @@
 from fabric.api import env, put, run
 import os
 
+# Remote hosts ip addresses
 env.hosts = ["54.236.56.204", "18.204.15.121"]
 
 
 def do_deploy(archive_path):
-    """ A function that deploys and decompresses code """
-    
-if not os.path.isfile(archive_path):
+    """ A function that deploys an archive to the web servers """
+
+    if not os.path.isfile(archive_path):
         return False
 
+    # Removing extension from the file name
     file_path1 = archive_path.split("/")[-1]
     dir_name = file_path.split(".")[0]
 
     dir_path = "/data/web_static/releases/{}".format(dir_name)
     try:
+        # Placing archive to temporary folder
         put(archive_path, "/tmp/")
+        # Directory to deploy code
         run("sudo mkdir -p {}".format(dir_path))
+        # Removing compression
         run("sudo tar -xvzf /tmp/{} -C {}".format(file_path1, dir_path))
         run("sudo rm /tmp/{}".format(file_path1))
+        # Moving files to a new folder
         run("sudo mv {}/web_static/* {}".format(dir_path, dir_path))
         run("sudo rm -rf {}/web_static".format(dir_path))
+        # Removing the old symbolic link and creating a new one
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {} /data/web_static/current".format(dir_path))
     except Exception as e:
+        # Return false if there is an error
         return False
     return True
