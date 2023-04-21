5dbb25240d304b29eb43c6aa23f676be7195c180
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-21 00:50:47 -0700

    Fixing the styling

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,20 +7,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """ A method to return Hello HBNB! """
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """ A method to return HBNB """
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
 def cTxt(text):
     """ Displaying C and the value of the text variable """
     return "C {}".format(text.replace("_", " "))
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
