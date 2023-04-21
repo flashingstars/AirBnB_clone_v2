f4b442952f9a6bf5a558065e7a5ee7ee88632cea
flashingstars <111060058+flashingstars@users.noreply.github.com>
2023-04-21 00:46:19 -0700

    Fixing the styling

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     """ A method to return Hello HBNB! """
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def index_hbnb():
     """ A method to return HBNB """
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
